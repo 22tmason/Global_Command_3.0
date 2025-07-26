20f3e3e4408dfa0bd4b933c767132fa91b695095
Tyler Mason <22tmason@wakatipu.school.nz>
2025-07-26 14:18:36 +1200

    Update so others can play

--- start_game.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Global Command 3.0 - Offline
-echo Starting offline server on http://localhost:8080
-cd /d "%~dp0"
-start "" http://localhost:8080
-miniweb\miniweb.exe -p 8080 -d miniweb\htdocs
-pause
